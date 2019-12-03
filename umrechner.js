e5393067a750ece8b7e738c3033f0607a4d780b1
Mia <54415454+miagudelj@users.noreply.github.com>
2019-12-03 15:01:46 +0100

    Add files via upload
    update files

--- umrechner.js
+++ umrechner.js
@@ -19,60 +19,60 @@ $(document).ready(function() {
 		var rz = $("#rz").val();
 		var rum = $("#rum").val();
 		var bw = $("#bw").val();
-		var kleidung = $("#kleidung").val();
+		var stoff = $("#stoff").val();
 		
 		// values
 		const dollarSkl = 50.25;
 		const dollarRum = 110.038;
 		const dollarRz = 0.23148;
 		const dollarBw = 5.39;
-		const dollarKleidung = 0.68508;
+		const dollarStoff = 0.68508;
 		
 		const brot = 0.24;
 		
 		var preis = $("#dollar").val();
 		
-		console.log("INFOS: " + sklaven + ";" + rz + ";" + rum + ";" + bw + ";" + kleidung);
+		console.log("INFOS: " + sklaven + ";" + rz + ";" + rum + ";" + bw + ";" + stoff);
 		
 		if (sklaven > 0) {
 			$("#dollar").val(sklaven * dollarSkl);
 			$("#rz").val(preis / dollarRz);
 			$("#rum").val(preis / dollarRum);
 			$("#bw").val(preis / dollarBw);
-			$("#kleidung").val(preis / dollarKleidung);
-			$("#brot").val(preis / brot);
+			$("#stoff").val(preis / dollarStoff);
+			$("#brot").val(preis / brot * 2);
 		}
 		else if (rz > 0)	{
 			$("#dollar").val(rz * dollarRz);
 			$("#skl").val(preis / dollarSkl);
 			$("#rum").val(preis / dollarRum);
 			$("#bw").val(preis / dollarBw);
-			$("#kleidung").val(preis / dollarKleidung);
-			$("#brot").val(preis / brot);
+			$("#stoff").val(preis / dollarStoff);
+			$("#brot").val(preis / brot * 2);
 		}
 		else if (rum > 0)	{
 			$("#dollar").val(rum * dollarRum);
 			$("#skl").val(preis / dollarSkl);
 			$("#rz").val(preis / dollarRz);
 			$("#bw").val(preis / dollarBw);
-			$("#kleidung").val(preis / dollarKleidung);
-			$("#brot").val(preis / brot);
+			$("#stoff").val(preis / dollarStoff);
+			$("#brot").val(preis / brot * 2);
 		}
 		else if (bw > 0)	{
 			$("#dollar").val(bw * dollarBw);
 			$("#skl").val(preis / dollarSkl);
 			$("#rz").val(preis / dollarRz);
 			$("#rum").val(preis / dollarRum);
-			$("#kleidung").val(preis / dollarKleidung);
-			$("#brot").val(preis / brot);
+			$("#stoff").val(preis / dollarStoff);
+			$("#brot").val(preis / brot * 2);
 		}
-		else if(kleidung > 0)	{
-			$("#dollar").val(kleidung * dollarKleidung);
+		else if(stoff > 0)	{
+			$("#dollar").val(stoff * dollarStoff);
 			$("#skl").val(preis / dollarSkl);
 			$("#rz").val(preis / dollarRz);
 			$("#rum").val(preis / dollarRum);
 			$("#bw").val(preis / dollarBw);
-			$("#brot").val(preis / brot);
+			$("#brot").val(preis / brot * 2);
 		}
 		else {
 			
